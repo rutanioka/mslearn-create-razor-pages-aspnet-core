1225a87a8e23d9d88e7556af51f5fa4a5087dcfc
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2023-07-29 06:15:58 +0000

    ,,,,

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,12 +1,22 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
+{
+    private readonly PizzaService _service;
+    public IList<Pizza> PizzaList { get;set; } = default!;
+
+    public PizzaListModel(PizzaService service)
     {
-        public void OnGet()
-        {
-        }
+        _service = service;
+    }
+
+    public void OnGet()
+    {
+        PizzaList = _service.GetPizzas();
     }
 }
